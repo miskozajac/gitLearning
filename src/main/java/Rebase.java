b617d67c940f837edc510ed673674b5b80b09deb
miskozajac <67467008+miskozajac@users.noreply.github.com>
2023-02-25 19:06:05 +0100

    m2

--- src/main/java/Rebase.java
+++ src/main/java/Rebase.java
@@ -2,6 +2,7 @@ public class Rebase {
     private String rebaseFeature;
     private String fds;
     private String gfd;
+    private String gdfhg;
 
     public void rozsirovanie() {
         System.out.println("fsgg");
