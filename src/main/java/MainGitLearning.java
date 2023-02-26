362c665aef4029d586a751f70b44c28d36d8cab7
miskozajac <67467008+miskozajac@users.noreply.github.com>
2023-02-26 11:44:34 +0100

    stash1

--- src/main/java/MainGitLearning.java
+++ src/main/java/MainGitLearning.java
@@ -1,6 +1,7 @@
 public class MainGitLearning {
     public static void main(String[] args) {
         System.out.println("master");
+        System.out.println("stash1");
         System.out.println("stash2");
     }
 }
